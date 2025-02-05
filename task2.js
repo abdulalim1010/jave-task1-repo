b6ec20e5b48c331aab75742447ba01a316ee19b6
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-05 19:30:23 +0600

    final test

---
+++ task2.js
@@ -0,0 +1,10 @@
+//input
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var bangla = 35.45;
+var physics = 99.50;
+sum= mathematics + biology + bangla + chemistry + physics;
+console.log(sum)
+avarage = sum / 5;
+console.log( avarage.toFixed(2))
