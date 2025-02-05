bc9df7b16d37c5e06a0274878170fe06154b2ac7
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-05 19:04:11 +0600

    fast task

--- introduction.js
+++ introduction.js
@@ -1,8 +1,15 @@
-var number = 56;
+/* var number = 56;
 var number2 = 45;
 var number3 = 89;
 sum = number2 + number2 + number3;
 
 console.log(sum)
 avrage = sum / 3;
-console.log(avrage)
+console.log(avrage) */
+var apple1Kg = 400;
+var orange1Kg = 300;
+sum = apple1Kg + orange1Kg;
+console.log(sum)
+var myTaka = 1000;
+subtraction = myTaka - sum;
+console.log(subtraction)
